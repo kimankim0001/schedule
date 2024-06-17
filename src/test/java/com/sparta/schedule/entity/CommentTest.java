7acfe901c0863fa8aa2e8d521812d4491f642b83
kinam <maltize9292@gmail.com>
2024-06-17 09:22:06 +0900

    Comment, comment 성공 테스트

--- src/test/java/com/sparta/schedule/entity/CommentTest.java
+++ src/test/java/com/sparta/schedule/entity/CommentTest.java
@@ -34,4 +34,18 @@ class CommentTest {
 
     }
 
+    @Test
+    @DisplayName("comment 성공 테스트")
+    void commentValidationSuccess(){
+        //given
+        Comment comment = new Comment();
+        comment.setUsername("테스트유저");
+        comment.setComment("테스트댓글");
+        //when then
+        assertAll(
+                () -> assertEquals("테스트댓글",comment.getComment(),"댓글을 입력해주세요")
+        );
+
+    }
+
 }
